eb85dc830c17a3cb0510bd47558282a95d35ccc5
Eugene <effectorea@yandex.ru>
2024-04-28 17:25:33 +0500

    created friend-request ui

--- src/app/home/components/connection-profile/connection-profile.component.ts
+++ src/app/home/components/connection-profile/connection-profile.component.ts
@@ -1,30 +1,67 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {BannerColorService} from "../../services/banner-color.service";
 import {ConnectionProfileService} from "../../services/connection-profile.service";
-import {ActivatedRoute} from "@angular/router";
-import {take} from "rxjs";
+import {ActivatedRoute, UrlSegment} from "@angular/router";
+import {map, Observable, Subscription, switchMap, take, tap} from "rxjs";
+import {User} from "../../../auth/models/user.model";
+import {FriendRequest_Status, FriendRequestStatus} from "../../models/friendRequest";
+import {environment} from "../../../../environments/environment";
 
 @Component({
   selector: 'app-connection-profile',
   templateUrl: './connection-profile.component.html',
   styleUrls: ['./connection-profile.component.scss'],
 })
-export class ConnectionProfileComponent  implements OnInit {
+export class ConnectionProfileComponent  implements OnInit, OnDestroy {
+
+  user: User;
+  friendRequestStatus: FriendRequest_Status;
+  friendRequestStatusSubscription$: Subscription;
+  userSubscription$: Subscription;
 
   constructor(public bannerColorService: BannerColorService, private connectionProfileService: ConnectionProfileService, private route: ActivatedRoute) { }
 
   ngOnInit() {
-    this.getUser()
+    this.friendRequestStatusSubscription$ = this.getFriendRequestStatus().pipe(
+      tap((friendRequestStatus: FriendRequestStatus) => {
+        this.friendRequestStatus = friendRequestStatus.status
+        this.userSubscription$ = this.getUser().subscribe((user: User) => {
+          this.user = user
+          const imagePath = user.imagePath ?? 'blank-user-image.jpg'
+          this.user['fullImagePath'] = `${environment.baseApiUrl}/feed/image/${imagePath}`
+          console.log(22, this.user['fullImagePath']);
+        })
+      })
+    ).subscribe()
   }
 
-  getUser() {
-    this.connectionProfileService.getConnectionUser(this.getUserIdFromUrl()).pipe(take(1)).subscribe((user) => {
-      console.log('Connection user', user);
-    })
+  getUser(): Observable<User> {
+    return this.getUserIdFromUrl().pipe(
+      switchMap((userId: number) => {
+        return this.connectionProfileService.getConnectionUser(userId)
+      })
+    )
   }
 
-  private getUserIdFromUrl(): number {
-    return +this.route.snapshot.paramMap.get('id')
+  getFriendRequestStatus(): Observable<FriendRequestStatus> {
+    return this.getUserIdFromUrl().pipe(
+      switchMap((id: number) => {
+        return this.connectionProfileService.getFriendRequestStatus(id)
+      })
+    )
+  }
+
+  private getUserIdFromUrl(): Observable<number> {
+    return this.route.url.pipe(
+      map((urlSegment: UrlSegment[]) => {
+        return +urlSegment[0].path
+      })
+    )
+  }
+
+  ngOnDestroy() {
+    this.userSubscription$.unsubscribe()
+    this.friendRequestStatusSubscription$.unsubscribe()
   }
 
 }
--- src/app/home/services/connection-profile.service.ts
+++ src/app/home/services/connection-profile.service.ts
@@ -3,6 +3,7 @@ import {HttpClient, HttpHeaders} from "@angular/common/http";
 import {Observable} from "rxjs";
 import {User} from "../../auth/models/user.model";
 import {environment} from "../../../environments/environment";
+import {FriendRequestStatus} from "../models/friendRequest";
 
 @Injectable({
   providedIn: 'root'
@@ -17,6 +18,10 @@ export class ConnectionProfileService {
 
   getConnectionUser(id: number): Observable<User>{
     return this.http.get<User>(`${environment.baseApiUrl}/user/${id}`)
+  }
+
+  getFriendRequestStatus(id: number): Observable<FriendRequestStatus> {
+    return this.http.get<FriendRequestStatus>(`${environment.baseApiUrl}/user/friend-request/status/${id}`)
   }
 
 
